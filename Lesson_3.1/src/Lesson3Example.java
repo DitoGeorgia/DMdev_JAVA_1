fa1ac3b7af49612ed2a2510d5a98407575acf809
DitoGeorgia <ditogeorgia@gmail.com>
2024-03-13 07:58:42 +0400

    add3

--- Lesson_3.1/src/Lesson3Example.java
+++
@@ -1,8 +0,0 @@
-public class Lesson3Example {
-    public static void main(String[] args) {
-        int value = 5;
-        int value2 = 3;
-        int sum = value + value2;
-        System.out.println(sum);
-    }
-}
