bed07b44a56cff7f8d63afddbac45f4dfa04087c
DitoGeorgia <ditogeorgia@gmail.com>
2024-03-13 05:18:56 +0400

    add

---
+++ Lesson_3.1/src/Lesson2Example.java
@@ -0,0 +1,28 @@
+public class Lesson2Example {
+    public static void main(String[] args) {
+        int temp;
+        temp = 567;
+        System.out.println(temp);
+
+        int value = 26;
+        value = temp;
+        System.out.println(value);
+
+        double value2 = 78.123;
+        System.out.println(value2);
+
+        boolean booleanValue = true;
+        System.out.println(booleanValue);
+
+        char testValue = 'a';
+        System.out.println(testValue);
+
+        testValue = 't';
+        System.out.println(testValue);
+
+        final char testValue2 = 'q';
+        System.out.println(testValue2);
+
+        testValue2 = 'w';
+    }
+}
