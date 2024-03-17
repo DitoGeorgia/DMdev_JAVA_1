aa5ac2cf41cdeaa4fd473087ce5d59bf1af1c7ee
DitoGeorgia <ditogeorgia@gmail.com>
2024-03-18 01:08:08 +0400

    add

--- Lesson_4/src/com/dmdev/lesson4/Lesson4Example.java
+++ Lesson_4/src/com/dmdev/lesson4/Lesson4Example.java
@@ -1,3 +1,5 @@
+package com.dmdev.lesson4;
+
 import java.util.Scanner;
 
 public class Lesson4Example {
---
+++ Lesson_4/src/com/dmdev/lesson4/SwitchExample.java
@@ -0,0 +1,26 @@
+package com.dmdev.lesson4;
+
+public class SwitchExample {
+    public static void main(String[] args) {
+//     byte, short, int, char, String, enum
+        int valeu = 2;
+        switch (valeu) {
+            case 1:
+                System.out.println("value == 1");
+                break;
+            case 2:
+                System.out.println("value == 2");
+                break;
+            case 3:
+                System.out.println("value == 3");
+                break;
+            case 4:
+            case 5:
+            case 6:
+                System.out.println("value == 4,5,6");
+                break;
+            default:
+                System.out.println("Нет такого значения");
+        }
+    }
+}
